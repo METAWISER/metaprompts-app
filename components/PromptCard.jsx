5dd06fd825892bc60e5488a5b47723c6ca89e1cd
Carlos <metawiser@gmail.com>
2023-07-30 17:46:47 +0200

    fix: unnesessary mongoose import.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 import Image from 'next/image'
 import {useSession} from 'next-auth/react'
 import { usePathname, useRouter } from 'next/navigation'
-import { set } from 'mongoose'
 
 
 export const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
